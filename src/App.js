23c69afc70787f475d38e0088f05bb28ae52ae3a
jmcaicedog <juanmanuelcaicedo@hotmail.com>
2023-03-08 11:21:00 -0500

    3 files modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import './style.css';
+import { Display } from "react-7-segment-display";
+import {useState} from "react";
 
 export default function App() {
+  const [value,setValue] = useState("1980");
+  const handlePlus = () =>{
+    setValue((parseInt(value)+1).toString())
+  }
+  const handleMinus = () =>{
+    setValue((parseInt(value)-1).toString())
+  }
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+    <div style={{backgroundColor:"#000000", display:"flex", flexDirection:"column"}}> 
+      <h1>CARZA S.A.</h1>
+      <Display height="150" count="4" value={value} />
+      <button onClick={handlePlus}>+</button>
+      <button onClick={handleMinus}>-</button>
     </div>
   );
 }
